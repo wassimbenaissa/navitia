4b522b051ed3249361d624c6473bd10f566552d9
xlqian <patrick.qian@kisio.com>
2022-04-04 19:22:02 +0200

    reap pt_planners sockets

--- source/jormungandr/jormungandr/pt_planners_manager.py
+++ source/jormungandr/jormungandr/pt_planners_manager.py
@@ -20,7 +20,7 @@ class PtPlannersManager(object):
         zmq_context,
         default_socket_path,
         db_configs_getter=None,
-        update_interval=60,
+        update_interval=1,
     ):
 
         self.pt_planners = {}
@@ -34,7 +34,7 @@ class PtPlannersManager(object):
         # For the sake of retro-compatibility, configs may be None
         configs = configs or {}
         self.logger = logging.getLogger(__name__)
-
+        self.old_configs = {}
         self.init(configs)
 
     def init(self, configs):
@@ -56,7 +56,11 @@ class PtPlannersManager(object):
             conf['args']['zmq_socket_type'] = self.zmq_socket_type
 
         for k in configs:
+            if self.old_configs.get(k) == configs[k]:
+                continue
+
             self.pt_planners[k] = utils.create_object(configs[k])
+            self.old_configs[k] = configs[k]
 
     def update_from_db(self):
         if not can_connect_to_database():
@@ -73,6 +77,9 @@ class PtPlannersManager(object):
         configs = self.db_configs_getter()
         self.init(configs)
         self.last_update = datetime.datetime.utcnow()
+
+    def get_all_pt_planners(self):
+        return self.pt_planners.items()
 
     def get_pt_planner(self, pt_planner_id):
         self.update_from_db()
